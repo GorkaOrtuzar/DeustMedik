af848230c9ce8255bc7e3ef5af0fef77e234d20b
Jorge Martinez Garcia <jorge.m@opendeusto.es>
2025-03-24 08:47:09 +0100

    Cambios en la clase medico, Creacion de la clase Persona y creacion de Paciente sin implementar

--- src/main/java/com/mycompany/modelo/Medico.java
+++ src/main/java/com/mycompany/modelo/Medico.java
@@ -8,59 +8,35 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Medico")
-public class Medico {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String Nombre;
-    private String Apellido;
-    private String Especialidad;
-    private String Contacto;
+public class Medico extends Persona {
+    
+    private String especialidad;
+    private String contacto;
 
     // No-argument constructor
     public Medico() {
+        super();
     }
 
-    public Medico(String Nombre, String Apellido, String Especialidad, String Contacto) {
-        this.Nombre = Nombre;
-        this.Apellido = Apellido;
-        this.Especialidad = Especialidad;
-        this.Contacto = Contacto;
-
+    public Medico(String dni, String nombre, String apellido, String telefono, String especialidad, String contacto) {
+        super(dni, nombre, apellido, telefono);
+        this.especialidad = especialidad;
+        this.contacto = contacto;
     }
 
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return Nombre;
-    }
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-    public String getApellido() {
-        return Apellido;
-    }
-    public void setApellido(String apellido) {
-        Apellido = apellido;
-    }
     public String getEspecialidad() {
-        return Especialidad;
+        return especialidad;
     }
     public void setEspecialidad(String especialidad) {
-        Especialidad = especialidad;
+        this.especialidad = especialidad;
     }
 
     public String getContacto() {
-        return Contacto;
+        return contacto;
     }
 
     public void setContacto(String contacto) {
-        Contacto = contacto;
+        this.contacto = contacto;
     }
 }
