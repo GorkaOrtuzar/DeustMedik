54b6b0a6e34ee49593f2c2f30e0ee899141600c7
naroaazcona <naroa.azcona@opendeusto.es>
2025-03-24 15:47:05 +0100

    Inserts BD

--- target/classes/HospitalBD.sql
+++ target/classes/HospitalBD.sql
@@ -37,3 +37,14 @@ CREATE TABLE Cita (
 	Motivo TEXT,
 	FOREIGN KEY (medico_id) REFERENCES Medico(id)    
 );
+
+INSERT INTO Medico (DNI, Nombre, Apellido, Especialidad, Contacto) VALUES
+('79230672L', 'Naroa', 'Azcona', 'Digestivo', 'naroa.azcona@opendeusto.es'),
+('79457623M', 'Gorka', 'Ortuzar', 'Endocrino', 'gorka.ortuzar@opendeusto.es'),
+('16784523R', 'Jacqueline', 'Furelos', 'Cabecera', 'jacqueline.furelos@opendeusto.es'),
+('30897639H', 'Jorge', 'Martinez', 'Cardiologia', 'jorge.martinez@opendeusto.es'),
+('30561342T', 'Ander', 'Perez', 'Hematologia', 'ander.martinez@opendeusto.es'),
+('79542098Q', 'Paula', 'Gonzalez', 'Urgencias', 'paula.gonzalez@opendeusto.es'),
+('79113006P', 'Ainhoa', 'Saez', 'Traumatologia', 'ainhoa.saez@opendeusto.es')
+
+
