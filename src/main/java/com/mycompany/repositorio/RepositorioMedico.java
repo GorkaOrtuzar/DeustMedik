f3e1498b4d99fef5c6b0869b0cdde7e3be80d59f
Jorge Martinez Garcia <jorge.m@opendeusto.es>
2025-03-24 16:26:05 +0100

    Creacion de la clase basica Horario y su repositorio + metodos en el repositorioMedico

--- src/main/java/com/mycompany/repositorio/RepositorioMedico.java
+++ src/main/java/com/mycompany/repositorio/RepositorioMedico.java
@@ -2,6 +2,15 @@ package com.mycompany.repositorio;
 
 import com.mycompany.modelo.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface RepositorioMedico extends JpaRepository<Medico, Long> {
+    List<Medico> findByEspecialidad(String especialidad);
+
+    List<Medico> findByDisponibilidadTrue();
+
+    Optional<Medico> findByCorreo(String correo);
+
+    boolean existsByIdAndCitasIsNotEmpty(Long id);
 }
