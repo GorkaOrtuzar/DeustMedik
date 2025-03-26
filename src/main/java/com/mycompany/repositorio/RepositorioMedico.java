93118bcd1d767f0eea754aed5c1d30a09ae58b8f
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2025-03-26 19:40:39 +0100

    Modificacion repositorio medicos

--- src/main/java/com/mycompany/repositorio/RepositorioMedico.java
+++ src/main/java/com/mycompany/repositorio/RepositorioMedico.java
@@ -2,30 +2,7 @@ package com.mycompany.repositorio;
 
 import com.mycompany.modelo.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-import java.util.Optional;
 
 public interface RepositorioMedico extends JpaRepository<Medico, Long> {
-    List<Medico> findByEspecialidad(String especialidad);
-
-    List<Medico> findByDisponibilidadTrue();
-
-    Optional<Medico> findByContacto(String contacto);
-
-    boolean existsByIdAndCitasIsNotEmpty(Long id);
-
-    @Query("SELECT DISTINCT m FROM Medico m " +
-       "JOIN m.horarios h " +
-       "WHERE h.dia = :fecha " +
-       "AND h.horaInicio <= :horaInicio " +
-       "AND h.horaFin >= :horaFin")
-    List<Medico> findMedicosDisponibles(@Param("fecha") LocalDate fecha, 
-                                    @Param("horaInicio") LocalTime horaInicio, 
-                                    @Param("horaFin") LocalTime horaFin);
-
 }
