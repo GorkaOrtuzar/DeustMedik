4d683fc168c2db8946cb84f666165e83c1ab8f21
Jorge Martinez Garcia <jorge.m@opendeusto.es>
2025-04-22 19:30:27 +0200

    Correccion y funcionamiento completo de las citas. Se guardam correctamente en BD

--- src/main/java/com/mycompany/controller/CitaController.java
+++ src/main/java/com/mycompany/controller/CitaController.java
@@ -2,13 +2,15 @@ package com.mycompany.controller;
 
 import com.mycompany.modelo.Cita;
 import com.mycompany.repositorio.RepositorioCita;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/citas")
+@RequestMapping("/api")
 public class CitaController {
 
     private final RepositorioCita repositorioCita;
@@ -21,4 +23,11 @@ public class CitaController {
     public List<Cita> obtenerTodasLasCitas() {
         return repositorioCita.findAll();
     }
+
+    @PostMapping("/citas")
+    public ResponseEntity<Cita> crearCita(@RequestBody Cita cita) {
+        Cita nueva = repositorioCita.save(cita);
+        return ResponseEntity.ok(nueva);
+    }
+
 }
