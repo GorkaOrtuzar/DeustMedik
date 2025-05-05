bba1bf3de76a719e2b5b0b729d1de52fbcb185f9
Jorge Martinez Garcia <jorge.m@opendeusto.es>
2025-05-05 18:43:57 +0200

    Perfil implementado

--- src/main/java/com/mycompany/controller/PacienteController.java
+++ src/main/java/com/mycompany/controller/PacienteController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.service.PacienteService;
 import com.mycompany.DTO.CredencialesDTO;
 import com.mycompany.modelo.Cita;
+import com.mycompany.modelo.Paciente;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -27,31 +29,40 @@ import io.swagger.v3.oas.annotations.tags.Tag;
    description = "Operaciones de login y autorización para pacientes"
 )
 public class PacienteController {
-    private final PacienteService pacienteService;
+   private final PacienteService pacienteService;
 
-    public PacienteController(PacienteService pacienteService) {
-        this.pacienteService = pacienteService;
-    }
+   public PacienteController(PacienteService pacienteService) {
+      this.pacienteService = pacienteService;
+   }
 
-    @Operation(
-       summary = "Login en el sistema",
-       description = "Permite a un paciente iniciar sesión proporcionando su DNI y contraseña. Devuelve un token si es exitoso.",
-       responses = {@ApiResponse(
-                 responseCode = "200",
-                 description = "OK: Inicio de sesión exitoso, devuelve un token"
-                 ), @ApiResponse(
-                 responseCode = "401",
-                 description = "Unauthorized: Credenciales inválidas, inicio de sesión fallido"
-                 )}
-    )
-    @PostMapping({"/login"})
-    public ResponseEntity<String> login(@RequestBody CredencialesDTO credentials) {
-       Optional<String> token = this.pacienteService.login(credentials.getDni(), credentials.getContrasenia());
-       return token.map(t -> 
-          new ResponseEntity<>(t, HttpStatus.OK)
-       ).orElseGet(() -> 
-          new ResponseEntity<>(HttpStatus.UNAUTHORIZED)
-       );
-    }
+   @Operation(
+      summary = "Login en el sistema",
+      description = "Permite a un paciente iniciar sesión proporcionando su DNI y contraseña. Devuelve un token si es exitoso.",
+      responses = {@ApiResponse(
+               responseCode = "200",
+               description = "OK: Inicio de sesión exitoso, devuelve un token"
+               ), @ApiResponse(
+               responseCode = "401",
+               description = "Unauthorized: Credenciales inválidas, inicio de sesión fallido"
+               )}
+   )
+
+   @PostMapping({"/login"})
+   public ResponseEntity<String> login(@RequestBody CredencialesDTO credentials) {
+      Optional<String> token = this.pacienteService.login(credentials.getDni(), credentials.getContrasenia());
+      return token.map(t -> 
+         new ResponseEntity<>(t, HttpStatus.OK)
+      ).orElseGet(() -> 
+         new ResponseEntity<>(HttpStatus.UNAUTHORIZED)
+      );
+   }
+
+   @GetMapping("/perfil")
+   public ResponseEntity<Paciente> perfil(@RequestParam(name = "dni") String dni) {
+      Paciente p = pacienteService.getPacienteByDNI(dni);
+      return p != null
+         ? ResponseEntity.ok(p)
+         : ResponseEntity.notFound().build();
+   }
 
 }
